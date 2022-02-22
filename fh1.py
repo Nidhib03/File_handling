ceeb664a1e7411a76ae6a0f1ed1c1e8b4cffacf4
NidhiB <100119660+Nidhib03@users.noreply.github.com>
2022-02-22 16:46:06 +0530

    Add files via upload

---
+++ fh1.py
@@ -0,0 +1,55 @@
+# basename function
+import os
+out = os.path.basename("/baz/foo")
+print(out)
+
+
+# dirname function
+import os
+out = os.path.dirname("/baz/foo")
+print(out)
+
+# isabs function - absolute path
+import os
+out = os.path.isabs("/baz/foo")
+print(out)
+
+# isdir function
+import os
+out = os.path.isdir("C:\\Users")
+print(out)
+
+
+# isfile function
+import os
+out = os.path.isfile("C:\\Users\foo.csv")
+print(out)
+
+
+# normcase function in windows
+import os
+out = os.path.normcase("/BAz")
+print(out)
+
+
+# normpath function in Unix
+import os
+out = os.path.normpath("foo/./bar")
+print(out)
+
+
+import os
+cwd = os.getcwd()
+print("Current working directory:", cwd)
+
+
+
+
+# Get the list of all files and directories in the root directory
+import os
+
+path = "/"
+dir_list = os.listdir(path)
+
+print("Files and directories in '", path, "' :")
+print(dir_list)

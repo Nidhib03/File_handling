ca55550d88a295b94bc80e09a8434f675f3f8319
NidhiB <100119660+Nidhib03@users.noreply.github.com>
2022-02-22 16:56:31 +0530

    Update fh4.py

--- fh4.py
+++ fh4.py
@@ -1,35 +1,28 @@
-# import os
-# print(os.name)
+import os
+print(os.name)    # posix
 
 
-# import os
-# fd = "file"
-
-# # popen() is similar to open()
-# file = open(fd, 'w')
-# file.write("Hello")
-# file.close()
-# file = open(fd, 'r')
-# text = file.read()
-# print(text)
-
-# # popen() provides a pipe/gateway and accesses the file directly
-# file = os.popen(fd, 'w')
-# file.write("Hello")
+ # os.popen(): This method opens a pipe to or from command
+fd = "file.txt"       
+# popen() is similar to open()
+file = open(fd, 'w')
+file.write("Hello")
+file.close()
+file = open(fd, 'r')
+text = file.read()
+print(text)
+# popen() provides a pipe/gateway and accesses the file directly
+file = os.popen(fd, 'w')
+file.write("Hello")
 # File not closed, shown in next function.
 
+ # removing the file.
+os.remove("file1")  
 
-
-# import os
-# os.remove("file1") #removing the file.
-
-import os
-
-result = os.path.exists("f_1") #giving the name of the file as a parameter.
+ # giving the name of the file as a parameter.
+result = os.path.exists("f_1") 
 print(result)
 
 
-import os
 size = os.path.getsize("f_1")
-
 print(f"Size of the file is {size} bytes.")
